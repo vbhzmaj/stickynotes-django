6e3a710148dabd2ec67bea67b5e2dae55c9522e7
vbhzmaj <33709353+vbhzmaj@users.noreply.github.com>
2024-06-08 12:33:52 +0200

    add functionality for update and delete

--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from .forms import NotesForm
 from .models import Snote
+
+class NotesDeleteView(DeleteView):
+    model = Snote
+    success_url = '/sticky/notes'
+    template_name='notes/snote_delete.html'
+
+class NotesUpdateView(UpdateView):
+    model = Snote
+    success_url = '/sticky/notes'
+    form_class = NotesForm
 
 class NotesCreateView(CreateView):
     model = Snote
