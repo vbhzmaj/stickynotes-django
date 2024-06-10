310bb5ceb5aa2771730e5fdf62aa39d7703b0249
vbhzmaj <33709353+vbhzmaj@users.noreply.github.com>
2024-06-10 11:02:24 +0200

    add user authentication and authorization

--- home/views.py
+++ home/views.py
@@ -3,11 +3,33 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = 'sticky/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
+
+
+class LoginInterfaceView(LoginView):
+    template_name = "home/login.html"
+
+class LogoutInterfaceView(LogoutView):
+    template_name = "home/logout.html"
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.today()}
-   
+       
 
 
 class AuthorizedView(LoginRequiredMixin, TemplateView):
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,12 @@
+from typing import Any
+from django.http.response import HttpResponseRedirect
+from django.db.models.query import QuerySet
+from django.forms import BaseModelForm
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin 
 
 from .forms import NotesForm
 from .models import Snote
@@ -10,21 +15,34 @@ class NotesDeleteView(DeleteView):
     model = Snote
     success_url = '/sticky/notes'
     template_name='notes/snote_delete.html'
-
+    
 class NotesUpdateView(UpdateView):
     model = Snote
     success_url = '/sticky/notes'
     form_class = NotesForm
+    login_url = "/admin"
 
-class NotesCreateView(CreateView):
+
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Snote
     success_url = '/sticky/notes'
     form_class = NotesForm
+    login_url = "/admin"
 
-class NotesListView(ListView):
+    def form_valid(self, form): 
+         self.object = form.save(commit=False)
+         self.object.user = self.request.user
+         self.object.save()
+         return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
     model = Snote
     context_object_name = "notes"
     template_name="notes/snote_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 
 class NotesDetailView(DetailView):
@@ -36,4 +54,4 @@ def detail(request, pk):
         note=Snote.objects.get(pk=pk)
     except Snote.DoesNotExist:
         raise Http404("Note doesn't exist")
-    return render(request, 'notes/notes_detail.html', {'note': note})
+    return render(request, 'notes/snote_detail.html', {'note': note})
